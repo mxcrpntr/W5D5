1411adcb399b87a644b3b04b8e9392443b896f87
Max Carpenter <carpenter.max@gmail.com>
2023-06-02 17:50:45 -0400

    first commit

---
+++ practice-for-ch-sql-movie-buff-long-practice/movie_buff/03_queries.rb
@@ -0,0 +1,43 @@
+def what_was_that_one_with(those_actors)
+  # Find the movies starring all `those_actors` (an array of actor names).
+  # Show each movie's title and id.
+  Movie.joins(:actors).where("actors.name": those_actors).group(:id).having("COUNT(movies.id) = ?", those_actors.length).select(:title,:id)
+
+
+end
+
+def golden_age
+  # Find the decade with the highest average movie score.
+  # HINT: Use a movie's year to derive its decade. Remember that you can use
+  # arithmetic expressions in SELECT clauses.
+  Movie.select("yr/10 AS decade").group("yr/10").order("AVG(score) DESC").limit(1).first.decade * 10
+end
+
+def costars(name)
+  # List the names of the actors that the named actor has ever appeared with.
+  # Hint: use a subquery
+  subquery = Movie.joins(:actors).where("actors.name = ?", name).select(:title)
+  subquery.joins(:actors).where("actors.name != ?", name).group("actors.name")
+end
+
+def actor_out_of_work
+  # Find the number of actors in the database who have not appeared in a movie.
+  
+end
+
+def starring(whazzername)
+  # Find the movies with an actor who had a name like `whazzername`. A name is
+  # like whazzername if the actor's name contains all of the letters in
+  # whazzername, ignoring case, in order.
+
+  # E.g., "Sylvester Stallone" is like "sylvester" and "lester stone" but not
+  # like "stallone sylvester" or "zylvester ztallone".
+  
+end
+
+def longest_career
+  # Find the 3 actors who had the longest careers (i.e., the greatest time
+  # between first and last movie). Order by actor names. Show each actor's id,
+  # name, and the length of their career.
+  
+end
